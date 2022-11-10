ca699504b536585b33f195fe8179f76ccdd1cebc
Jason Kai <tkai@uwo.ca>
2022-11-10 16:24:11 -0500

    formatting and lint fixes for python scripts
    - Refactored python scripts to use functions
    - Performed linting and quality fixes with isort, black, and flake8

--- workflow/scripts/save_connmap_template_npz.py
+++ workflow/scripts/save_connmap_template_npz.py
@@ -1,17 +1,36 @@
 import nibabel as nib
 import numpy as np
-mask_nib = nib.load(snakemake.input.mask)
-mask_vol = mask_nib.get_fdata()
-mask_indices = mask_vol > 0 
-masked = mask_vol[mask_indices]
-
-nvoxels = len(masked)
-ntargets = len(snakemake.params.connmap_3d)
-conn = np.zeros((nvoxels,ntargets))
-for i,conn_file in enumerate(snakemake.params.connmap_3d):
-    vol = nib.load(conn_file).get_fdata()
-    masked =  vol[mask_indices].T
-    conn[:,i] = masked
-np.savez(snakemake.output.connmap_npz, conn=conn,mask=mask_vol,affine=mask_nib.affine)
 
 
+def save_connmap_template(mask, connmap_3d, connmap_npz):
+    # Load mask and get volume
+    mask_nii = nib.load(mask)
+    mask_vol = mask_nii.get_fdata()
+    mask_indices = mask_vol > 0
+    masked = mask_vol[mask_indices]
+
+    # Get number of voxels in mask and connectivity map
+    nvoxels = len(masked)
+    ntargets = len(connmap_3d)
+
+    # Gather and save template connectivity
+    conn = np.zeros((nvoxels, ntargets))
+    for i, conn_file in enumerate(connmap_3d):
+        vol = nib.load(conn_file).get_fdata()
+        masked = vol[mask_indices].T
+        conn[:, i] = masked
+
+    np.savez(
+        connmap_npz,
+        conn=conn,
+        mask=mask_vol,
+        affine=mask_nii.affine,
+    )
+
+
+if __name__ == "__main__":
+    save_connmap_template(
+        mask=snakemake.input.mask,
+        connmap_3d=snakemake.params.connmap_3d,
+        connamp_npz=snakemake.output.connmap_npz,
+    )
